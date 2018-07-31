e9245dda62d10184988d119fc6cce0347b68d4e6
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-07-31 20:14:56 -0300

    Usando Função

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 from kivy.app import App
-from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
+from kivy.properties import ObjectProperty
+
+
+class incrementador(BoxLayout):
+    pass
 
 class Teste(App):
 
     def build(self):
-        box = BoxLayout(orientation="vertical")
-        button = Button(text="Botão 1", font_size=30, on_release=self.incrementar)
-        self.label = Label(text="Texto 1", font_size=30)
-        box.add_widget(button)
-        box.add_widget(self.label)
+        self.x=0
+        return incrementador()
 
-        self.x = 0
-
-        return box
-
-    def incrementar(self, button):
+    def incrementar(self, *args):
         self.x += 1
-        self.label.text = str(self.x)
-
+        return self.x
 
 Teste().run()
